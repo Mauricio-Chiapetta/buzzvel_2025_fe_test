7694374e7c483b7ee0d74363683e8e3294b7a6ff
Mauricio Chiapetta <m.faisca1234@gmail.com>
2025-04-28 20:57:19 -0300

    feat: Responsive Navbar.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,16 @@
 import Image from "next/image";
 import logo from "@/assets/images/Logo-colored.png";
 import Link from "next/link";
-import { ChevronDown } from "lucide-react";
+import { ChevronDown, Menu } from "lucide-react";
 
 export function Navbar() {
   return (
     <div className="bg-white">
-      <div className="px-[80px]">
+      <div className="px-[16px] sm:px-[80px]">
         <div className="py-[24px] flex items-center justify-between">
           <div className="flex items-center gap-[48px]">
             <Image src={logo} alt="Logo" className="w-[103px] h-[35px]" />
-            <nav className="flex gap-[48px]">
+            <nav className=" hidden sm:flex gap-[48px]">
               <Link href={"#"} className="text-blue font-medium">
                 Products
               </Link>
@@ -25,21 +25,23 @@ export function Navbar() {
                 className="text-blue font-medium flex items-center gap-2"
               >
                 Resources
-                <ChevronDown size={20} className="hover:animate-bounce" />
+                <ChevronDown size={20} />
               </Link>
             </nav>
           </div>
-          <div className="flex items-center gap-[16px]">
+          <div className="hidden sm:flex items-center gap-[16px]">
             <Link href={"#"} className="text-blue font-medium">
               Log In
             </Link>
             <Link
               href={"#"}
               className="border-[2px] text-blue font-medium py-2 px-4 border-blue rounded-[8px] hover:bg-blue hover:text-white transition-colors duration-300"
-              
             >
               Sign Up Now
             </Link>
+          </div>
+          <div className="sm:hidden flex">
+            <Menu className="text-blue" />
           </div>
         </div>
       </div>
